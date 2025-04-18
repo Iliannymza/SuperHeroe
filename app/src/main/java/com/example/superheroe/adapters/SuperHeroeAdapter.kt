588a9764d2f6fe972076ca1d8664ad1ed18c5dca
Iliannymza <mendoza71997@hmail.com>
2025-04-18 19:31:17 +0200

    contruccion del binding

--- app/src/main/java/com/example/superheroe/adapters/SuperHeroeAdapter.kt
+++ app/src/main/java/com/example/superheroe/adapters/SuperHeroeAdapter.kt
@@ -1,12 +1,10 @@
 package com.example.superheroe.adapters
 
-import android.icu.text.Transliterator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.motion.widget.MotionScene.Transition.TransitionOnClick
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.superheroe.R
